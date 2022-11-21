6205b64f150235c1c88d397398592f2032a51251
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-11-21 11:37:30 +0530

    edit functionality added

--- src/main/java/com/ems/controller/EmployeeController.java
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.ems.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ems.model.Employee;
@@ -11,12 +17,14 @@ import com.ems.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService service;
-	
+
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model m) {
+		List<Employee> emp = service.getAllEmployee();
+		m.addAttribute("emp",emp);
 		return "EmployeeIndex";
 	}
 
@@ -24,10 +32,20 @@ public class EmployeeController {
 	public String addEmp() {
 		return "AddEmployee";
 	}
-	
+
 	@PostMapping("/insertEmployeeData")
-	public String empInsertData(@ModelAttribute Employee e) {
+	public String empInsertData(@ModelAttribute Employee e, HttpSession session) {
 		service.addEmp(e);
-		return "EmployeeIndex";
+		session.setAttribute("msg", "Employee Added successfully..");
+		return "AddEmployee";
 	}
+	
+	@GetMapping("edit/{id}")
+	public String edit(@PathVariable int id, Model m) {
+		
+		Employee e = service.getEmpById(id);
+		m.addAttribute("emp", e);
+		return "Add";
+	}
+	
 }
--- src/main/java/com/ems/service/EmployeeService.java
+++ src/main/java/com/ems/service/EmployeeService.java
@@ -1,5 +1,8 @@
 package com.ems.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +20,15 @@ public class EmployeeService {
 		
 	}
 
+	public List<Employee> getAllEmployee(){
+		return repo.findAll();
+	}
+	
+	public Employee getEmpById(int id) {
+		Optional<Employee> e = repo.findById(id);
+		if(e.isPresent()) {
+			return e.get();
+		}
+		return null;
+	}
 }
